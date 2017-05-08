70e56f95dc309f9b5795900db46e89e567fc28bd
Andy Mantell <andymantell@hotmail.com>
2017-05-08 11:53:05 +0100

    Tweak the task registration slightly

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 var glob = require('glob')
 var path = require('path')
 
-module.exports = function (gulp, config) {
+module.exports = (function (gulp, config) {
   var tasks = glob.sync(path.join(__dirname, '/tasks/**/*.js'))
+  var taskFunctions = {}
 
   tasks.forEach(function (task) {
-    var taskModule = require(task)
-    taskModule(gulp, config)
+    taskFunctions[task] = require(task)
   })
-}
+
+  return taskFunctions
+})()
