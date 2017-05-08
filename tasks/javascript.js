7778ebbe760056b9d340f00bad6188eb797a5080
Andy Mantell <andymantell@hotmail.com>
2017-05-08 14:23:12 +0100

    Add custom rollup warning handler to suppress warnings about external dependencies

--- tasks/javascript.js
+++ tasks/javascript.js
@@ -25,6 +25,13 @@ module.exports = function(gulp, config) {
           moduleContext: config.moduleContext,
           legacy: true,
           entry: entrypoint,
+          onwarn: function (warning) {
+            if ( warning.code === 'UNRESOLVED_IMPORT' ) return;
+
+            // Replace with next(warning) when warn handlers are chainable
+            // See https://github.com/rollup/rollup/issues/1245
+            console.log(warning.message)
+          },
           plugins: [
             nodeResolve(),
             commonjs(),
