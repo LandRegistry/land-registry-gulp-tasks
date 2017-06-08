7dba0c4c8b9a9e9489b43e9bdab7ab42b197dbd0
Andy Mantell <andymantell@hotmail.com>
2017-06-08 09:22:57 +0100

    Fix browsersync task so that it breaks the skeleton's caches

--- tasks/watch.js
+++ tasks/watch.js
@@ -4,12 +4,31 @@ var browserSync = require('../utils/browsersync')
 
 module.exports = function(gulp, config) {
   gulp.task('watch', function () {
-    gulp.watch(path.join(config.assetsPath, 'src/scss/**/*.scss'), ['sass', 'sass-lint'])
-    gulp.watch(path.join(config.assetsPath, 'src/javascripts/**/*.js'), ['js', 'standardjs'])
+    gulp.watch(path.join(config.assetsPath, 'src/scss/**/*.scss'), ['sass'])
+    gulp.watch(path.join(config.assetsPath, 'src/javascripts/**/*.js'), ['js'])
     gulp.watch('gulp/**/*.js', ['standardjs'])
 
+    var unixEpoch = 'Thursday, 01 Jan 1970 00:00:00 GMT'
+
     browserSync.init({
-      proxy: config.localhost,
+      proxy: {
+        target: config.localhost,
+        proxyReq: [
+          function(proxyReq) {
+            proxyReq.setHeader('Pragma', 'no-cache')
+            proxyReq.setHeader('Cache-Control', 'no-cache')
+            proxyReq.setHeader('If-Modified-Since', unixEpoch)
+          }
+        ],
+        proxyRes: [
+          function(proxyRes, req, res) {
+            proxyRes.headers.expires =  unixEpoch
+            proxyRes.headers['last-modified'] = 'Thursday, 01 Jan 2970 00:00:00 GMT'
+            proxyRes.headers['cache-control'] = 'public, max-age=0'
+            delete proxyRes.headers.etag
+          }
+        ]
+      },
       open: false
     })
 
